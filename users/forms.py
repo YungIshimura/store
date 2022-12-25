ced4b1211d30a9111b46610e79fbebce1f9a9284
Mark Tereshchenko <83189636+YungIshimura@users.noreply.github.com>
2022-12-25 22:01:11 +0200

    Add widgets in forms

--- users/forms.py
+++ users/forms.py
@@ -1,9 +1,21 @@
 from django.contrib.auth.forms import AuthenticationForm
 from .models import User
+from django import forms
 
 
 class UserLoginForm(AuthenticationForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder': 'Введите имя пользователя'
+    }))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Введите пароль'
+    }))
 
     class Meta:
         model = User
         fields = ('username', 'password')
+
+    def __init__(self, *args, **kwargs):
+        super(UserLoginForm, self).__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control py-4'
